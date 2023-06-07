fd28977ecab1e8368e636222b4e02bb8a4253bbd
Kiono78 <benoit.terral@gmail.com>
2023-06-07 15:50:08 +0000

    ex00 day02

--- Day2/ex00/load_csv.py
+++ Day2/ex00/load_csv.py
@@ -3,8 +3,11 @@ import os
 
 
 def load(path: str) -> pd.DataFrame:
-    if os.path.exists(path) is None:
+    if os.path.exists(path) is False or os.path.isdir(path) is True:
         return None
-    dataframe = pd.read_csv('life_expectancy_years.csv')
-    print(dataframe.shape)
+    try:
+        dataframe = pd.read_csv(path)
+    except Exception as e:
+        raise ValueError(f"An Error occured while reading csv: {e}")
+    print(f"Loading dataset of dimensions {dataframe.shape}")
     return dataframe
--- Day2/ex00/tester.py
+++ Day2/ex00/tester.py
@@ -2,4 +2,18 @@ from load_csv import load
 import os
 
 os.chdir(os.path.dirname(__file__))
-print(load("life_expectancy_years.csv"))
+
+try:
+    print(load("../ex00"))
+except ValueError as e:
+    print(f"Exception: {e}")
+
+try:
+    print(load("../../Day1/ex05/landscape.jpg"))
+except ValueError as e:
+    print(f"Exception: {e}")
+
+try:
+    print(load("life_expectancy_years.csv"))
+except ValueError as e:
+    print(f"Exception: {e}")
